80413587bcb38631cfdabd22d0c4ffc42ab4a626
IT-markaz <it_center33@mail.ru>
2023-09-28 18:40:42 +0500

    uyga vazifagacha qismi

---
+++ script.js
@@ -0,0 +1,5 @@
+const view = document.querySelector(".view");
+const add = document.querySelector(".add");
+const title = document.querySelector(".title");
+const price = document.querySelector(".price-value");
+const korzina = document.querySelector(".korzina");
